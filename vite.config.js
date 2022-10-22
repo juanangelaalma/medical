830e9188aadb6b74b14f0074e4971a920a56a6d1
juanangelaalma <juanlah77@gmail.com>
2022-10-22 07:00:24 +0700

    fix bug vite production

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ const path = require('path')
 export default {
   root: path.resolve(__dirname, 'src'),
   build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html'),
+        contact: path.resolve(__dirname, 'src/pages/contact.html'),
+        about: path.resolve(__dirname, 'src/pages/about.html'),
+      }
+    },
     outDir: path.resolve(__dirname, 'dist'),
   },
   resolve: {
